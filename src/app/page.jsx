6f6805ecca56db74f86609d9e31f39b2966cb180
jose luis gomez angarita <74441811+joseluiscodesk8@users.noreply.github.com>
2024-02-23 08:18:53 -0500

    megumi

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+
+export default function Home() {
+  return (
+    <main>
+      <Image src="https://i.ibb.co/FYFjT6t/megumi.jpg" alt="logo" width={380} height={250} />
+    </main>
+  );
+}
