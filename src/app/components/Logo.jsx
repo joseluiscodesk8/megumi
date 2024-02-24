6f1b2a581448f2d12e6e7d192824905c3cd00792
jose luis gomez angarita <74441811+joseluiscodesk8@users.noreply.github.com>
2024-02-24 09:20:10 -0500

    megumi2

--- src/app/components/Logo.jsx
+++ src/app/components/Logo.jsx
@@ -4,7 +4,7 @@ import styles from "../styles/index.module.scss";
 const Logo = () => {
     return(
         <picture className={styles.logo}>
-            <Image src="https://i.ibb.co/FYFjT6t/megumi.jpg" alt="logo" width={200} height={120} loading="lazy"/>
+            <Image src="https://i.ibb.co/bdgFRqV/megumi.png" alt="logo" width={200} height={120} loading="lazy"/>
         </picture>
     )
 } 
