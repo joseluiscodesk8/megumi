be2c3c40aa22d976a2780e1f37b25bdef5eaed7f
jose luis gomez angarita <74441811+joseluiscodesk8@users.noreply.github.com>
2024-02-26 08:48:39 -0500

    megumi4

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,21 +1,30 @@
 import Image from "next/image";
 import styles from "./styles/index.module.scss";
-import menuData from "./data/menus.json"
+import menuData from "./data/menus.json";
+
 
 export default function Home() {
   return (
-    <div className={styles.menuContainer}>
-    <h1>Menú</h1>
-    <div className={styles.menuItems}>
-      {menuData.menu.map(item => (
-        <div key={item.nombre} className={styles.menuItem}>
-          <Image src={item.imagen} alt={item.nombre} width={280} height={250} loading="lazy" />
-          <h2>{item.nombre}</h2>
-          <p>Precio: ${item.precio}</p>
-          <p>{item.descripcion}</p>
-        </div>
-      ))}
-    </div>
-  </div>
+    <main className={styles.menuContainer}>
+      <h1>Entradas</h1>
+      <section>
+        {menuData.menu.map((item) => (
+          <div key={item.nombre} className={styles.menuItems}>
+            <Image
+              src={item.imagen}
+              alt={item.nombre}
+              width={280}
+              height={250}
+              loading="lazy"
+            />
+            <div>
+              <h2>{item.nombre}</h2>
+              <p>Precio: ${item.precio}</p>
+            </div>
+            <p>{item.descripcion}</p>
+          </div>
+        ))}
+      </section>
+    </main>
   );
 }
