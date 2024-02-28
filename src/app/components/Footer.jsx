d9de548ec8097a2edb3798acb00c9a0fbc62d647
jose luis gomez angarita <74441811+joseluiscodesk8@users.noreply.github.com>
2024-02-28 09:03:49 -0500

    megumi9

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,15 +1,20 @@
 import styles from "../styles/index.module.scss"
+import { FaInstagram, FaFacebook, FaTiktok, FaPhoneAlt } from 'react-icons/fa';
+import { RiWhatsappLine } from 'react-icons/ri'; 
+
+
 
 const Footer = () => {
   return (
     <footer className={styles.Footer}>
-      <span>Instagram</span>
-      <span>Facebook</span>
-      <span>Tik Tok</span>
-      <span>315 4358290</span>
-      <span>75492302</span>
+      <span><FaInstagram /> megumisushi</span>
+      <span><FaFacebook /> megumi.sushi0213</span>
+      <span><FaTiktok /> megumisushi.cucuta</span>
+      <span><RiWhatsappLine /> 315 4358290</span>
+      <span><FaPhoneAlt /> 75492302</span>
     </footer>
   );
 };
 
 export default Footer;
+
