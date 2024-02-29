110c36288f5f117df63ed617bdc1acf00e09e582
jose luis gomez angarita <74441811+joseluiscodesk8@users.noreply.github.com>
2024-02-29 18:45:36 -0500

    megumi10

--- src/app/especialidades/page.jsx
+++ src/app/especialidades/page.jsx
@@ -1,11 +1,44 @@
+'use client'
+
+import Image from "next/image";
 import styles from "../styles/index.module.scss";
+import menuData from "../data/especiales.json";
+import { motion } from "framer-motion";
+import dynamic from "next/dynamic";
+
+const DynamicImage = dynamic(() => (import('../ui/LazyImage')));
 
 const Especialidades = () => {
-    return(
-        <main className={styles.home}>
-        <div>megumi especialidades</div>
-      </main>
-    )
+  return (
+    <main className={styles.menuContainer}>
+      <h1>Especialidades</h1>
+      <h2>Todos nuestros arroces vienen en peso de un kilo para compartir</h2>
+      <section>
+        {menuData.menu.map((item, index) => (
+          <motion.div  key={item.nombre}
+            className={styles.menuItems}
+            initial={{ opacity: 0, y: 50 }} // Establece la posición inicial y la opacidad
+            animate={{ opacity: 1, y: 0 }} // Establece la animación de entrada
+            transition={{ duration: 0.5, delay: index * 0.1 }}>
+            <picture>
+            <DynamicImage
+              src={item.imagen}
+              alt={item.nombre}
+              width={280}
+              height={250}
+              loading="lazy"
+            />
+            </picture>
+            <div>
+              <h2>{item.nombre}</h2>
+              <p>${item.precio}</p>
+            </div>
+            <p className={styles.description}>{item.descripcion}</p>
+          </motion.div>
+        ))}
+      </section>
+    </main>
+  );
 }
 
 export default Especialidades;
