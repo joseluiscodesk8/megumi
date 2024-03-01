7749c21c136b8e7300864aadfd36f964f49dd142
jose luis gomez angarita <74441811+joseluiscodesk8@users.noreply.github.com>
2024-03-01 17:42:40 -0500

    megumi11

--- src/app/roll/page.jsx
+++ src/app/roll/page.jsx
@@ -1,11 +1,43 @@
+'use client'
+
+import Image from "next/image";
 import styles from "../styles/index.module.scss";
+import menuData from "../data/roll.json";
+import { motion } from "framer-motion";
+import dynamic from "next/dynamic";
+
+const DynamicImage = dynamic(() => (import('../ui/LazyImage')));
 
 const Roll = () => {
-    return(
-        <main className={styles.home}>
-        <div>megumi Roll Cortante</div>
-      </main>
-    )
+  return (
+    <main className={styles.menuContainer}>
+      <h1>Roll Corcante</h1>
+      <section>
+        {menuData.menu.map((item, index) => (
+          <motion.div  key={item.nombre}
+            className={styles.menuItems}
+            initial={{ opacity: 0, y: 50 }} // Establece la posición inicial y la opacidad
+            animate={{ opacity: 1, y: 0 }} // Establece la animación de entrada
+            transition={{ duration: 0.5, delay: index * 0.1 }}>
+            <picture>
+            <DynamicImage
+              src={item.imagen}
+              alt={item.nombre}
+              width={280}
+              height={250}
+              loading="lazy"
+            />
+            </picture>
+            <div>
+              <h2>{item.nombre}</h2>
+              <p>${item.precio}</p>
+            </div>
+            <p className={styles.description}>{item.descripcion}</p>
+          </motion.div>
+        ))}
+      </section>
+    </main>
+  );
 }
 
 export default Roll;
